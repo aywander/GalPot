00f0289407200f0358b4d9b1d5aad140401929ab
Alex Y. Wagner <alexander.y.wagner@gmail.com>
2020-05-14 18:45:26 +0900

    Added some of my helper modules, continued work on notebook

---
+++ mpl_aesth.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from cycler import cycler
+
+def adjust_rcParams(style='seaborn', use_kpfonts=False, dark_mode=False):
+
+    plt.style.use(style)
+
+    if dark_mode:
+        fc = 'white'
+        fc_i = 'black'
+        fc_n = (1, 1, 1, 0.3)
+        # TODO: Make the colorpalette below a bit lighter
+        tableau10_colors = ['006BA4', 'FF800E', 'ABABAB', '595959', '5F9ED1', 'C85200', '898989', 'A2C8EC', 'FFBC79',
+                            'CFCFCF']
+    else:
+        fc = 'black'
+        fc_i = 'white'
+        fc_n = (0, 0, 0, 0.1)
+        tableau10_colors = ['006BA4', 'FF800E', 'ABABAB', '595959', '5F9ED1', 'C85200', '898989', 'A2C8EC', 'FFBC79',
+                            'CFCFCF']
+
+    plt.rcParams['axes.prop_cycle'] = cycler(color=['#' + s for s in tableau10_colors])
+
+    plt.rcParams.update({
+        'text.color': fc,
+        'axes.labelcolor': fc,
+        'xtick.color': fc,
+        'ytick.color': fc,
+        'xtick.direction': 'in',
+        'ytick.direction': 'in',
+        'grid.color': fc_i,
+        # 'axes.axisbelow': False,
+        'grid.alpha': 0.5,
+        'axes.facecolor': fc_n,
+        # 'axes.grid.which': 'both',
+        # 'axes.grid.axis': 'both',
+        'figure.facecolor': (0, 0, 0, 0),
+        # 'figure.edgecolor': 'black',
+        'savefig.facecolor': (0, 0, 0, 0),
+    })
+
+    if use_kpfonts:
+        plt.rcParams.update({
+            'font.family': 'serif',
+            'text.usetex': True,
+            'text.latex.preamble': [
+                r'\usepackage{amsmath}',
+                r'\usepackage{amssymb}',
+                r'\usepackage{siunitx}',
+                r'\usepackage[notextcomp]{kpfonts}',
+            ],
+        })
+
+    else:
+        plt.rcParams.update({
+            'font.family': 'serif',
+            'font.serif': 'Times New Roman',
+            'font.sans-serif': 'Times New Roman',
+            'mathtext.fontset': 'cm',
+        })
+
